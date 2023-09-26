ef0470ba3323db4777ac4194eb1351787bac1b22
gidambayiantony <gidambayiantony@gmail.com>
2023-09-26 14:11:53 -0700

    app-icons

---
+++ components/app-icons/Footer.js
@@ -0,0 +1,19 @@
+import { Box, Link, Text } from '@chakra-ui/react';
+import { FaGooglePlay, FaAppStore } from 'react-icons/fa'; // Import icons from a popular icon library
+
+function Footer() {
+  return (
+    <Box as="footer" textAlign="center" padding="4rem">
+      <Text>Copyright © 2023 YooKatale</Text>
+      <Link href="https://play.google.com/store/apps/details?id=com.yookatale">
+        <FaGooglePlay size={32} color="green" />
+      </Link>
+      <Link href="https://apps.apple.com/app/yookatale/id1234567890">
+        <FaAppStore size={32} color="blue" /> 
+      </Link>
+    </Box>
+  );
+}
+
+export default Footer;
+
