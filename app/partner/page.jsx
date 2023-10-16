124afa22a8b232b785a3c7bc68cc2d0fedc96b72
bigmosi <kinyeramo@gmail.com>
2023-10-16 17:56:48 +0300

    added vendor form

---
+++ app/partner/page.jsx
@@ -0,0 +1,143 @@
+"use client";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Select,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import VendorForm from "@components/DeliveryForm";
+import ButtonComponent from "@components/Button";
+import { DB_URL } from "@config/config";
+
+const Partner = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    email: "",
+    transport: "bike",
+  });
+  const [isLoading, setLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(`${DB_URL}/vendor/new`, formData);
+      setSuccessMessage('Form submitted successfully!');
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 5000);
+    } catch (error) {
+      console.error("An error occurred:", error);
+      onSubmit("An unexpected error occurred");
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+  <div className="mb-10">
+    <VendorForm />
+    <Box p={4} borderWidth={1} borderRadius="lg" w={{ base: "100%", md: "md" }} mx="auto"> {/* Set the width to full on mobile and medium on larger screens */}
+      <form onSubmit={handleSubmit}>
+       <p className="text-3xl text-left mb-4 text-dark">Fill out the vendor form</p>
+       <Stack spacing={4}>
+            {/* Vendor Information */}
+            <FormControl id="name">
+              <FormLabel>Vendor's Name*</FormLabel>
+              <Input
+                type="text"
+                name="name"
+                placeholder="Vendor's Name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="border border-dark rounded hover:border-red focus:border-red px-2 py-1 italic"
+              />
+            </FormControl>
+            <FormControl id="address">
+              <FormLabel>Address*</FormLabel>
+              <Input
+                type="text"
+                name="address"
+                placeholder="Address"
+                value={formData.address}
+                onChange={handleChange}
+                required
+                className="border border-dark rounded hover:border-red focus:border-red px-2 py-1 italic"
+              />
+            </FormControl>
+            <FormControl id="phoneNumber">
+              <FormLabel>Phone Number*</FormLabel>
+              <Input
+                type="text"
+                name="phone"
+                placeholder="Phone Number"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+                className="border border-dark rounded hover:border-red focus:border-red px-2 py-1 italic"
+              />
+            </FormControl>
+            <FormControl id="emailAddress">
+              <FormLabel>Email Address*</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="border border-dark rounded hover:border-red focus:border-red px-2 py-1 italic"
+              />
+            </FormControl>
+
+            {/* Delivery Information */}
+            <FormControl id="transport">
+              <FormLabel>Transport*</FormLabel>
+              <Select
+                name="transport"
+                value={formData.transport}
+                onChange={handleChange}
+                required
+                className="border border-dark rounded hover:border-red focus:border-red px-2 py-1 italic"
+              >
+                <option value="bike">Bike</option>
+                <option value="vehicle">Vehicle</option>
+                <option value="motorcycle">Motorcycle</option>
+              </Select>
+            </FormControl>
+
+            <div className="text-center md:text-left">
+              <ButtonComponent
+                text={"Sign Up"}
+                size={"lg"}
+                type={"submit"}
+                icon={isLoading && <Loader2 size={20} />}
+              />
+            </div>
+          </Stack>
+        {successMessage && (
+          <div className="text-dark text-center mt-4">{successMessage}</div>
+        )}
+      </form>
+    </Box>
+  </div>
+  );
+};
+
+export default Partner;
+
--- components/DeliveryForm.jsx
+++ components/DeliveryForm.jsx
@@ -1,28 +1,50 @@
-"use client";
+"use client"
 import { Box, FormControl, FormLabel, Input, Button, Select } from '@chakra-ui/react';
 import { useState } from 'react';
 import { DB_URL } from '@config/config';
 import axios from 'axios';
+import ButtonComponent from './Button';
+import { Loader2 } from 'lucide-react';
 
-const DeliveryForm = () => {
-  const [formData, setFormData] = useState({
-    fullname: '',
-    phone: '',
-    email: '',
-    location: '',
-    businessName: '',
-    businessAddress: '',
-    businessHours: '',
-    transport: 'bike',
-  });
-  const [successMessage, setSuccessMessage] = useState('');
+const VendorForm = ({ onSubmit }) => {
+  const [fullname, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [location, setLocation] = useState('');
+  const [businessName, setBusinessName] = useState('');
+  const [businessAddress, setBusinessAddress] = useState('');
+  const [businessHours, setBusinessHours] = useState('');
+  const [transport, setTransport] = useState("bike");
+  const [successMessage, setSucessMessage] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post(`${DB_URL}/partner/new`, formData);
-      setSuccessMessage('Form submitted successfully!');
-      // Clear form fields here
+      await axios.post(`${DB_URL}/partner/new`, {
+        fullname,
+        phone,
+        email,
+        location,
+        businessName,
+        businessAddress,
+        businessHours,
+        transport,
+      });
+      setName('');
+      setPhone('');
+      setBusinessName('');
+      setBusinessAddress('');
+      setBusinessHours('');
+      setLocation('');
+      setTransport('bike');
+      setSucessMessage('Form submitted successfully!');
+      setTimeout(() => {
+        setSucessMessage('');
+      }, 5000);
+    
+
     } catch (error) {
       console.error('Error creating partner', error);
     }
@@ -30,41 +52,118 @@ const DeliveryForm = () => {
 
   return (
     <Box className="mx-auto p-4 bg-black mb-20 mt-20">
-      <p className="text-3xl text-left mb-4 text-dark">Fill out the form to start delivering</p>
-      <form onSubmit={handleSubmit}>
-        <FormControl className="mb-4">
-          <FormLabel>Name*</FormLabel>
-          <Input
-            type="text"
-            value={formData.fullname}
-            onChange={(event) => setFormData({ ...formData, fullname: event.target.value })}
-            placeholder="Name Lastname"
-            required
-          />
-        </FormControl>
-        {/* Add similar fields for Phone number, Location, Email address, Business name, Business address, Business hours */}
-        <FormControl className="mb-4">
-          <FormLabel>Transport*</FormLabel>
-          <Select
-            value={formData.transport}
-            onChange={(event) => setFormData({ ...formData, transport: event.target.value })}
-            required
-          >
-            <option value="bike">Bike</option>
-            <option value="vehicle">Vehicle</option>
-            <option value="motorcycle">Motorcycle</option>
-          </Select>
-        </FormControl>
-        <Button type="submit" colorScheme="teal" className="bg-green-500 mt-4">
-          Sign up
-        </Button>
-        {successMessage && (
-          <div className="text-dark text-center mt-4">{successMessage}</div>
-        )}
-      </form>
+      <div className="flex flex-col lg:flex-row">
+        <div className="p-4 rounded-xl bg-white">
+          <p className="text-3xl text-left mb-4 text-dark">Fill out the form to start delivering</p>
+          <form onSubmit={handleSubmit}>
+            <FormControl className="mb-4">
+              <FormLabel>Name*</FormLabel>
+              <Input
+                className="border-b border-dark italic hover:border-red focus:border-red px-2 py-1"
+                value={fullname}
+                onChange={(event) => setName(event.target.value)}
+                placeholder="Name Lastname"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Phone number*</FormLabel>
+              <Input
+                className="border border-dark italic hover:border-red focus:border-red rounded px-2 py-1"
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
+                placeholder="0712378472"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Location*</FormLabel>
+              <Input
+                className="border border-dark italic hover:border-red focus:border-red rounded px-2 py-1"
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+                placeholder="Please enter your location"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Email address*</FormLabel>
+              <Input
+                className="border border-dark italic hover:border-red focus:border-red rounded px-2 py-1"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="example@gmail.com"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Business name*</FormLabel>
+              <Input
+                className="border border-dark italic hover:border-red focus:border-red rounded px-2 py-1"
+                value={businessName}
+                onChange={(event) => setBusinessName(event.target.value)}
+                placeholder="Please enter your business name"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Business address*</FormLabel>
+              <Input
+                className="border border-dark italic hover:border-red focus:border-red rounded px-2 py-1"
+                value={businessAddress}
+                onChange={(event) => setBusinessAddress(event.target.value)}
+                placeholder="Please enter business address"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Business hours*</FormLabel>
+              <Input
+                className="border border-dark italic hover:border-red focus:border-red rounded px-2 py-1"
+                value={businessHours}
+                onChange={(event) => setBusinessHours(event.target.value)}
+                placeholder="Please enter business hours"
+                required
+              />
+            </FormControl>
+            <FormControl className="mb-4">
+              <FormLabel>Transport*</FormLabel>
+              <Select
+                className="border border-dark rounded hover:border-red focus:border-red px-2 py-1 italic"
+                value={transport}
+                onChange={(event) => setTransport(event.target.value)}
+                required
+              >
+                <option value="bike">Bike</option>
+                <option value="vehicle">Vehicle</option>
+                <option value="motorcycle">Motorcycle</option>
+              </Select>
+            </FormControl>
+             <div className="text-center md:text-left">
+                  <ButtonComponent
+                    text={"Sign Up"}
+                    size={"lg"}
+                    type={"button"}
+                    icon={isLoading && <Loader2 size={20} />}
+                  />
+             </div>
+             {successMessage && (
+              <div className="text-dark text-center mt-4">{successMessage}</div>
+            )}
+          </form>
+        </div>
+        <div className="p-4 w-full md:w-1/2 md:ml-5">
+          <div className="mt-10">
+            <h2 className="text-3xl text-white">Hello</h2>
+            <p className="text-white mt-5">
+              Do you want to set your own schedule and connect when it suits you? Get paid for delivering orders with the yookatale Courier App.
+            </p>
+            <h4 className="text-white font-bold mt-5">Sign up today!</h4>
+          </div>
+        </div>
+      </div>
     </Box>
   );
 };
 
-export default DeliveryForm;
-
+export default VendorForm;
